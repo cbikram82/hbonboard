7b078bf38ed3f239f0ba2ae829b5b4d511e00b5f
cbikram82 <bikram82@gmail.com>
2023-02-09 23:09:52 +0000

    hb committ 2

---
+++ index.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $('#btn').on('click', function(event) {
+        
+        // alert("working!!"+$('#email').val());
+        
+        var url = 'https://qssqlhxrof574kkug7gwvn7wsu0eoljh.lambda-url.us-east-1.on.aws/?email='+$('#email').val()+'&password='+$('#pwd').val();
+        // var url = "https://www.google.com/?email="+$('#email').val()+'&password='+$('#pwd').val();  
+        
+        $.get(url, function(data, status){
+            location.href=data+'.html'
+            alert(data);
+            alert(status);
+        });
+          
+   })   
+});
+   
