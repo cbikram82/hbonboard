a7d6b66f669b3de70e971f26390f822588516d16
cbikram82 <bikram82@gmail.com>
2023-02-13 12:48:10 +0000

    added node folder

--- index.mjs
+++ index.mjs
@@ -1,15 +1,20 @@
+
+//const {dbq} = require('./dbquery.js');
+//import { dbquery } from "./dbquery.mjs";
 $( document ).ready(function() {
     $('#btn').on('click', function(event) {
         
         // alert("working!!"+$('#email').val());
-        
+       // const data = import {dbq.dbquery($('#email').val())};
+       
+
         var url = 'https://qssqlhxrof574kkug7gwvn7wsu0eoljh.lambda-url.us-east-1.on.aws/?email='+$('#email').val()+'&password='+$('#pwd').val();
         // var url = "https://www.google.com/?email="+$('#email').val()+'&password='+$('#pwd').val();  
         
         $.get(url, function(data, status){
-            //location.href=data+'.html'
-            alert(data);
-            alert(status);
+            location.href=data+'.html'
+           // alert(data);
+           // alert(status);
         });
           
    })   
