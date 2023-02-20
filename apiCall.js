172dcbaa186e23ad6877ad665cf75125f991c133
cbikram82 <bikram82@gmail.com>
2023-02-20 10:51:52 +0000

    changes to print status post apiCall

--- apiCall.js
+++ apiCall.js
@@ -1,26 +1,24 @@
-require('./dbtest.js');
-const express = require('express');
-const cors = require('cors');
-const { apifunct } = require('./dbtest.js');
+require("./dbtest.js");
+const express = require("express");
+const cors = require("cors");
+const { apifunct } = require("./dbtest.js");
 //const { dbquery } = require('./dbtest.js');
 
 const app = express();
 app.use(cors());
 
-app.get('/user/:id', (req, res) => {
-     
-    const request = req.params.id;
-    //dbquery("bikramemp@test.com");
-    var result =  apifunct(request)
-    .then(r=>{
-        console.log("r=" + r.role);
-        res.send(r.role);
-    });
-    console.log("here is the result : " + result);
-    //res.send(result);
+app.get("/user/:id", (req, res) => {
+  const request = req.params.id;
+  var result = apifunct(request).then((r) => {
+    console.log("r=" + r.role);
+    res.send(r.role);
+  });
+  console.log("here is the result : " + result);
+  //res.send(result);
 });
 
-app.listen(9229, () => {
-    console.log('Express app listening on port 9229');
-});
+//app.listen(9229, () => {
+//   console.log('Express app listening on port 9229');
+//});
 
+module.exports = app;
--- database.js
+++ database.js
@@ -1,31 +1,33 @@
-const {Client} = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
-    host: 'sbadb-1.c8ikdnliwh3x.us-east-1.rds.amazonaws.com',
-    port: 5432,
-    user: 'sbapostgres',
-    password: 'Ruhi0707#',
-    database: 'postgres',
+  host: "sbadb-1.c8ikdnliwh3x.us-east-1.rds.amazonaws.com",
+  port: 5432,
+  user: "sbapostgres",
+  password: "Ruhi0707#",
+  database: "postgres",
 });
-const usr = 'bikramemp@test.com';
+const usr = "bikramemp@test.com";
 //checkCreds(usr);
 client
   .connect()
-  .then(() => console.log('connected'))
-  .catch((err) => console.error('connection error', err.stack))
+  .then(() => console.log("connected"))
+  .catch((err) => console.error("connection error", err.stack));
 
-  var result = client.query('SELECT role FROM hbuser WHERE username = $1', [usr], (err, res) =>{
-    if(!err){
-        console.log(res.rows);
-    }else{
-        console.log(err.message);
+var result = client.query(
+  "SELECT role FROM hbuser WHERE username = $1",
+  [usr],
+  (err, res) => {
+    if (!err) {
+      console.log(res.rows);
+    } else {
+      console.log(err.message);
     }
     client.end((err) => {
-        console.log('client has disconnected')
-        if (err) {
-          console.log('error during disconnection', err.stack)
-        }
-      })
-    
-})
-
+      console.log("client has disconnected");
+      if (err) {
+        console.log("error during disconnection", err.stack);
+      }
+    });
+  }
+);
