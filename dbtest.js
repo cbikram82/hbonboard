4ec26e72f891034c3597d4a5df50c1db17c38518
cbikram82 <bikram82@gmail.com>
2023-02-20 10:52:15 +0000

    api call to return status

--- dbtest.js
+++ dbtest.js
@@ -15,49 +15,9 @@ async function dbquery(usr) {
     .then(() => console.log("connected"))
     .catch((err) => console.error("connection error", err.stack));
 
-    // await client.query(
-    //   "SELECT role FROM hbuser WHERE username = $1",
-    //   [usr],
-    //   (err, res) => {
-    //     if (!err) {
-    //       console.log("result is " + res.rows);
-    //       x = res.rows[0].role;
-    //       console.log("x=" + x);
-    //     } else {
-    //       console.log(err.message);
-    //     }
-    //     client.end((err) => {
-    //       console.log("client has disconnected");
-    //       if (err) {
-    //         console.log("error during disconnection", err.stack);
-    //       }
-    //     });
-    //   }
-    // );
-
-  //var result = client.query()
-
   const text = "SELECT role FROM hbuser WHERE username = $1";
   const value = [usr];
 
-//   // callback
-//   client.query(text, value, (err, res) => {
-//     if (err) {
-//       console.log(err.stack);
-//     } else {
-//       console.log(res.rows[0]);
-//     }
-//   });
-
-  // promise
-  //   client
-  //     .query(text, value)
-  //     .then(res => {
-  //       console.log(res.rows[0])
-  //     })
-  //     .catch(e => console.error(e.stack))
-
-  // async/await
   try {
     const res = await client.query(text, value);
     console.log(res.rows[0]);
@@ -73,7 +33,6 @@ async function dbquery(usr) {
   });
 
   //console.log("returning results" + x);
-  
 }
 
 module.exports = {
--- main-index.js
+++ main-index.js
@@ -1,31 +1,29 @@
-
 //const {db} = require('.dbtest');
 
+$(document).ready(function () {
+  $("#btn").on("click", function (event) {
+    alert("working!!" + $("#email").val());
+    // const data = import {dbq.dbquery($('#email').val())};
+    //"http://localhost:9229/user/"
+    //console.log(res1);
+    var email = $.get(
+      "https://r5v1ktsk87.execute-api.us-east-1.amazonaws.com/dev/" +
+        $("#email").val(),
+      function (data, status) {
+        location.href = data + ".html";
+        //    // alert(data);
+            alert(status);
+      }
+    );
+    // const data = "employee";
+    //location.href=data+'.html';
+    // var url = 'https://qssqlhxrof574kkug7gwvn7wsu0eoljh.lambda-url.us-east-1.on.aws/?email='+$('#email').val()+'&password='+$('#pwd').val();
+    // var url = "https://www.google.com/?email="+$('#email').val()+'&password='+$('#pwd').val();
 
-$( document ).ready(function() {
-    $('#btn').on('click', function(event) {
-        
-        alert("working!!"+$('#email').val());
-       // const data = import {dbq.dbquery($('#email').val())};
-       
-       //console.log(res1);
-       var email = 
-       $.get("http://localhost:9229/user/"+$('#email').val(),  function(data, status){
-            location.href=data+'.html'
-        //    // alert(data);
-        //    // alert(status);
-         });
-      // const data = "employee";
-       //location.href=data+'.html';
-       // var url = 'https://qssqlhxrof574kkug7gwvn7wsu0eoljh.lambda-url.us-east-1.on.aws/?email='+$('#email').val()+'&password='+$('#pwd').val();
-        // var url = "https://www.google.com/?email="+$('#email').val()+'&password='+$('#pwd').val();  
-        
-        // $.get(url, function(data, status){
-        //     location.href=data+'.html'
-        //    // alert(data);
-        //    // alert(status);
-        // });
-          
-   })   
+    // $.get(url, function(data, status){
+    //     location.href=data+'.html'
+    //    // alert(data);
+    //    // alert(status);
+    // });
+  });
 });
-   
