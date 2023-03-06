e25848196a0aa9d4fb7c21bf362c0c80bf95a3a4
cbikram82 <bikram82@gmail.com>
2023-03-06 15:51:07 +0000

    commented res send results outside api function call.

--- main-index.js
+++ main-index.js
@@ -7,23 +7,12 @@ $(document).ready(function () {
     //"http://localhost:9229/user/"
     //console.log(res1);
     var email = $.get(
-      "http://localhost:9229/user/" +
-        $("#email").val(),
+      "http://localhost:9229/user/" + $("#email").val(),
       function (data, status) {
         location.href = data + ".html";
         //    // alert(data);
-            //alert(status);
+        //alert(status);
       }
     );
-    // const data = "employee";
-    //location.href=data+'.html';
-    // var url = 'https://qssqlhxrof574kkug7gwvn7wsu0eoljh.lambda-url.us-east-1.on.aws/?email='+$('#email').val()+'&password='+$('#pwd').val();
-    // var url = "https://www.google.com/?email="+$('#email').val()+'&password='+$('#pwd').val();
-
-    // $.get(url, function(data, status){
-    //     location.href=data+'.html'
-    //    // alert(data);
-    //    // alert(status);
-    // });
   });
 });
